7aa8ad1eff4f3e80abbf7aa4d0dee16c1dacaa2f
zhanhb <6323014+zhanhb@users.noreply.github.com>
2022-01-25 09:39:26 +0800

    Update AnnotationContext

--- jnc-runtime/src/main/java/jnc/provider/AnnotationContext.java
+++ jnc-runtime/src/main/java/jnc/provider/AnnotationContext.java
@@ -18,16 +18,21 @@ package jnc.provider;
 import java.lang.annotation.Annotation;
 import java.lang.reflect.AnnotatedElement;
 import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Objects;
 import javax.annotation.Nullable;
 
 /**
  * @author zhanhb
  */
+@SuppressWarnings({"FinalClass", "PackageVisibleInnerClass"})
 interface AnnotationContext {
 
     static AnnotationContext newContext(AnnotatedElement element) {
-        return new AnnotationContextImpl(element.getAnnotations());
+        return new Simple(element.getAnnotations());
     }
 
     static AnnotationContext[] newMethodParameterContexts(Method method) {
@@ -35,13 +40,13 @@ interface AnnotationContext {
         int length = parameterAnnotations.length;
         AnnotationContext[] annotationContexts = new AnnotationContext[length];
         for (int i = 0; i < length; ++i) {
-            annotationContexts[i] = new AnnotationContextImpl(parameterAnnotations[i]);
+            annotationContexts[i] = new Simple(parameterAnnotations[i]);
         }
         return annotationContexts;
     }
 
     static AnnotationContext newMockContext(List<Class<? extends Annotation>> annotations, AnnotationContext previous) {
-        return new CombineAnnotationContext(new AnnotationContextImpl(annotations.stream()
+        return new Combine(new Simple(annotations.stream()
                 .map(klass -> SimpleAnnotationBuilder.of(klass).build())
                 .toArray(Annotation[]::new)), previous);
     }
@@ -52,5 +57,54 @@ interface AnnotationContext {
 
     @Nullable
     <T extends Annotation> T getAnnotation(Class<T> annotationClass);
+
+    final class Simple implements AnnotationContext {
+
+        private final Map<Class<?>, Annotation> annotations;
+
+        Simple(Annotation[] annotations) {
+            @SuppressWarnings(value = "CollectionWithoutInitialCapacity")
+            Map<Class<?>, Annotation> map = new LinkedHashMap<>();
+            for (Annotation annotation : annotations) {
+                map.putIfAbsent(annotation.annotationType(), annotation);
+            }
+            for (Annotation annotation : annotations) {
+                for (Annotation transfer : annotation.annotationType().getAnnotations()) {
+                    map.putIfAbsent(transfer.annotationType(), transfer);
+                }
+            }
+            this.annotations = map;
+        }
+
+        @Nullable
+        @SuppressWarnings("unchecked")
+        @Override
+        public <T extends Annotation> T getAnnotation(Class<T> annotationClass) {
+            return (T) annotations.get(annotationClass);
+        }
+
+    }
+
+    final class Combine implements AnnotationContext {
+
+        private final AnnotationContext[] upstream;
+
+        Combine(AnnotationContext... upstream) {
+            Arrays.stream(upstream).forEach(Objects::requireNonNull);
+            this.upstream = upstream;
+        }
+
+        @Override
+        public <T extends Annotation> T getAnnotation(Class<T> annotationClass) {
+            for (AnnotationContext annotationContext : upstream) {
+                T annotation = annotationContext.getAnnotation(annotationClass);
+                if (annotation != null) {
+                    return annotation;
+                }
+            }
+            return null;
+        }
+
+    }
 
 }
